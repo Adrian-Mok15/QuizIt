02beb13faf6d29077e629c6d3ec46883a2eabc29
Jesus Navarro <72825053+Jesus-Maza@users.noreply.github.com>
2021-11-24 16:25:56 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/quizit/User.java
@@ -0,0 +1,37 @@
+package com.example.instagram;
+
+import com.parse.ParseClassName;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+
+@ParseClassName("User")
+public class User extends ParseObject {
+
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_RANK = "rank";
+    public static final String KEY_USER  = "User";
+    public static final String KEY_SCORE ="score";
+    public static final String KEY_PROFILE_PICTURE = "Profile_picture";
+
+    public ParseFile getProfile_picture(){
+        return getParseFile(KEY_PROFILE_PICTURE);
+    }
+    public String getScore(){
+        return getString(KEY_SCORE);
+    }
+    public void setScore(String score){
+        put(KEY_SCORE, score);
+    }
+    public String getRank(){
+        return getString(KEY_RANK);
+    }
+    public void setRank(String rank){
+        put(KEY_RANK, rank);
+    }
+    public String getUsername(){
+        return getString(KEY_USERNAME);
+    }
+    public void setUsername(String username){
+        put(KEY_USERNAME, username);
+    }
+}
