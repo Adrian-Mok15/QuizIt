431af1784724cc9dd55d691b8674d9fd0bcfb28a
Robert Bangiyev <rbangiyev123@gmail.com>
2021-11-30 23:39:46 -0500

    Adjusted leaderboard look

--- app/src/main/java/com/example/quizit/LeaderboardAdapter.java
+++ app/src/main/java/com/example/quizit/LeaderboardAdapter.java
@@ -14,6 +14,7 @@ import com.bumptech.glide.Glide;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
 
+import java.text.DecimalFormat;
 import java.util.List;
 
 public class LeaderboardAdapter extends RecyclerView.Adapter<LeaderboardAdapter.ViewHolder> {
