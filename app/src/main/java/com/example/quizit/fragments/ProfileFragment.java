e81b323bb24206638cfbee375a7e8e890dcb058f
Jesus Navarro <72825053+Jesus-Maza@users.noreply.github.com>
2021-11-30 23:16:45 -0500

    Update ProfileFragment.java

--- app/src/main/java/com/example/quizit/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/quizit/fragments/ProfileFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,10 +19,19 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.example.quizit.Leaderboard2;
 import com.example.quizit.LoginActivity;
+import com.example.quizit.QuizAttempt;
 import com.example.quizit.R;
 import com.example.quizit.SettingsActivity;
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
